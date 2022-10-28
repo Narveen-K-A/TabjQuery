83be7d72e84ca3fffef43e2ef665cdcfb0f24b88
Narveen K A <narveen@techversantinfotech.com>
2022-10-28 17:51:54 +0530

    Commit

---
+++ js/tab.js
@@ -0,0 +1,17 @@
+$('#tabs div a:not(:first)').addClass('inactive');
+$('.container').hide();
+$('.container:first').show();
+$('#tabs div a').click(function(){
+	var t = $(this).attr('href');
+	$('#tabs div a').addClass('inactive');        
+	$(this).removeClass('inactive');
+	$('.container').hide();
+	$(t).fadeIn('slow');
+	return false;
+})
+if($(this).hasClass('inactive')){
+	$('#tabs div a').addClass('inactive');         
+	$(this).removeClass('inactive');
+	$('.container').hide();
+	$(t).fadeIn('slow');    
+}
